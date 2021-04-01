d7aebf9d91756b2e8974b21855f358fbdcf86b06
povsister <pov@mahou-shoujo.moe>
2021-04-01 15:09:20 +0800

    add doc.go

---
+++ doc.go
@@ -0,0 +1,11 @@
+/*
+
+Production-ready Secure Copy Protocol (aka: SCP) implemented in Go with
+well documentation and neat dependency.
+
+Package scp act exactly same with a scp command in shell, but providing a programmatic,
+full-featured and pure-go scp implementation for you.
+
+*/
+
+package scp
